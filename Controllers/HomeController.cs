db6cc54287ef767a32ff4f720cfdbff0ed64f4be
deepak0499 <53998424+deepak0499@users.noreply.github.com>
2022-11-04 10:54:47 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Models;
+using AzureSearch.Models;
+using AzureSearch.SearchInterface;
+using AzureSearch.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AzureSearch.Models.SearchData;
+
+namespace AzureSearch.Controllers
+{
+    /// <summary>
+    /// Loads the first page
+    /// </summary>
+    public class HomeController : Controller
+    {
+        private readonly ISearch ctx;
+        public HomeController(ISearch _ctx)
+        {
+            ctx = _ctx;
+        }
+
+        /// <summary>
+        /// Reload the 1st page
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            var model = ctx.PrepareInitialData().Result;
+            model = ctx.PrepareFacetData(model);
+            return View(model);
+        }
+
+        /// <summary>
+        /// Reload the 2nd page
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<ActionResult> Index(SearchData model)
+        {
+            try
+            {
+                int page;
+                var CategoryFilter = string.Empty;
+                var TagsFilter = string.Empty;
+                ctx.InitSearch();
+                if (model.paging != null && model.paging == "next")
+                {
+                    page = (int)TempData["page"] + 1;
+                    model.searchText = TempData["searchfor"].ToString();
+                }
+                else
+                {
+                    if (model.searchText == null)
+                    {
+                        model.searchText = "*";
+                    }
+                    page = 0;
+                }
+                if (Request.Form["AmenityFacet"].Count > 0)
+                {
+                    TagsFilter = ctx.AzureSearchFilterLikeCondition(new List<string>(Request.Form["AmenityFacet"]), "Tags", false);
+                }
+
+                if (Request.Form["CategoryFacet"].Count > 0)
+                {
+                    CategoryFilter = ctx.AzureSearchFilterLikeCondition(new List<string>(Request.Form["CategoryFacet"]), "Category", false);
+                }
+                await ctx.SearchHelper(model, TagsFilter, CategoryFilter,page);
+                var InitalModel = ctx.PrepareInitialData().Result;
+                InitalModel = ctx.PrepareFacetData(InitalModel);
+                model.AmenityFacet = InitalModel.AmenityFacet;
+                model.CategoryFacet = InitalModel.CategoryFacet;
+                TempData["page"] = page;
+                TempData["searchfor"] = model.searchText;
+                model = ctx.PreSetFacets(model, Request.Form["CategoryFacet"], Request.Form["AmenityFacet"]);
+            }
+            catch (Exception ex)
+            {
+                return View("Error", new ErrorViewModel { RequestId = "1" });
+            }
+
+            return View("Index", model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Service/SearchService.cs
@@ -0,0 +1,183 @@
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Azure.Search.Documents.Models;
+using AzureSearch.Models;
+using AzureSearch.SearchInterface;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureSearch.Service
+{
+    public class SearchService : ISearch
+    {
+        // Local variables
+        private readonly string SearchServiceUri;
+        private readonly string SearchServiceQueryApiKey;
+        private static SearchIndexClient _indexClient;
+        private static SearchClient _searchClient;
+        public SearchService(IConfiguration configuration)
+        {
+            SearchServiceUri= configuration.GetSection("SearchServiceSetting").GetSection("SearchServiceUri").Value;
+            SearchServiceQueryApiKey = configuration.GetSection("SearchServiceSetting").GetSection
+                ("SearchServiceQueryApiKey").Value;
+        }
+
+
+        /// <summary>
+        /// Prepare the Data for the Facet
+        /// </summary>
+        /// <param name="DataModel"></param>
+        /// <returns></returns>
+        public SearchData PrepareFacetData(SearchData DataModel)
+        {
+
+            List<string> facetTag = DataModel.resultList.Facets["Tags"].Select(x => x.Value.ToString()).ToList();
+
+            var JoinStringTag = string.Join(",", facetTag).Split(",");           
+            var ListAmenities = new List<AmenityFacetModel>();
+
+            for (int i = 0; i < JoinStringTag.Length; i++)
+            {             
+                ListAmenities.Add(new AmenityFacetModel { FacetName = FirstLetterToUpper(JoinStringTag[i].Trim()), FacetIndex = i, FacetCount = 1 });
+            }
+
+            var DistinctGroupAmenities = ListAmenities.GroupBy(x => x.FacetName).Select(y => new { FacetName = y.Key, Count = y.Sum(z => z.FacetCount) });
+            var facetAmenity = DistinctGroupAmenities.Select(x => new SelectListItem { Text = x.FacetName, Value = x.Count.ToString() }).ToList();
+
+            var facetCategory = DataModel.resultList.Facets["Category"].Select(x => new SelectListItem { Text = x.Value.ToString(), Value = x.Count.ToString() }).ToList();
+
+            DataModel.CategoryFacet = facetCategory;
+            DataModel.AmenityFacet = facetAmenity;
+            return DataModel;
+        }
+        /// <summary>
+        /// Prepare the Initial Data
+        /// </summary>
+        /// <returns></returns>
+        public async Task<SearchData> PrepareInitialData()
+        {
+            InitSearch();
+            SearchOptions options = new SearchOptions();
+            options.Facets.Add("Tags,count:6");
+            options.Facets.Add("Category,count:5");
+
+            SearchData model = new SearchData();
+            SearchResults<Hotel> searchResult = await _searchClient.SearchAsync<Hotel>("*", options);         
+            model.resultList = searchResult;
+            return model;
+        }
+        /// <summary>
+        /// Changes the 1st letter to uppercase
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+                return null;
+
+            if (str.Length > 1)
+                return char.ToUpper(str[0]) + str.Substring(1);
+
+            return str.ToUpper();
+        }
+
+        /// <summary>
+        /// PreSetFacets
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="saveChecks"></param>
+        public SearchData PreSetFacets(SearchData model, string[] CategoryFacet, string[] AmenitiesFacet)
+        {
+
+            foreach (var item in CategoryFacet)
+            {
+                model.CategoryFacet.Where(x => x.Text == item).FirstOrDefault().Selected = true;
+            }
+            foreach (var item in AmenitiesFacet)
+            {
+                model.AmenityFacet.Where(x => x.Text == item).FirstOrDefault().Selected = true;
+            }
+            return model;
+        }
+
+        /// <summary>
+        /// AzureSearchFilterLikeCondition
+        /// </summary>
+        /// <param name="values"></param>
+        /// <param name="field"></param>
+        /// <param name="exclude"></param>
+        /// <returns></returns>
+        public string AzureSearchFilterLikeCondition(List<string> values, string field, bool exclude)
+        {
+            var result = "";
+            var builder = new StringBuilder();
+            if (!exclude)
+            {
+                if (values != null && values.Count > 0)
+                {
+                    var filtersValuesClubbed = string.Join(",", values);
+                    builder.Append($"search.ismatch('{filtersValuesClubbed}','{ field }')");
+                }
+            }
+            else
+            {
+                if (values != null && values.Count > 0)
+                {
+                    var filtersValuesClubbed = string.Join(",", values);
+                    builder.Append($"not search.ismatch('{filtersValuesClubbed}','{ field }')");
+                }
+            }
+            result = builder.ToString();
+            if (string.IsNullOrWhiteSpace(result) && values != null)
+            {
+                result = "";
+            }
+            return result;
+        }
+
+        public async Task<SearchData> SearchHelper(SearchData model,string TagsFilter,string CategoryFilter,int page)
+        {
+            var options = new SearchOptions
+            {
+
+                SearchMode = SearchMode.All,
+                Skip = page * GlobalVariables.ResultsPerPage,
+                Size = GlobalVariables.ResultsPerPage,
+                IncludeTotalCount = true,
+            };
+            options.Select.Add("HotelName");
+            options.Select.Add("Description");
+            options.Select.Add("Tags");
+            options.Select.Add("Category");
+            options.Facets.Add("Tags,count:6");
+            options.Facets.Add("Category,count:5");
+            var filterbuilder = new StringBuilder();
+            filterbuilder.Append(string.IsNullOrEmpty(TagsFilter) ? "" : filterbuilder.Length > 0 ? $"{TagsFilter}" : $"{TagsFilter}");
+            filterbuilder.Append(string.IsNullOrEmpty(CategoryFilter) ? "" : filterbuilder.Length > 0 ? $" and {CategoryFilter}" : $"{CategoryFilter}");
+            options.Filter = filterbuilder.ToString();
+            model.resultList = await _searchClient.SearchAsync<Hotel>(model.searchText, options);
+            return model;
+        }
+
+        /// <summary>
+        /// To setup the search client and all the configurations
+        /// </summary>
+        public SearchClient InitSearch()
+        {
+         //   _builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+         //   _configuration = _builder.Build();
+         //   string searchServiceUri = _configuration["SearchServiceUri"];
+         //   string queryApiKey = _configuration["SearchServiceQueryApiKey"];
+            _indexClient = new SearchIndexClient(new Uri(SearchServiceUri), new AzureKeyCredential(SearchServiceQueryApiKey));
+            _searchClient = _indexClient.GetSearchClient("hotels-search-index");
+            return _searchClient;
+        }
+    }
+}
